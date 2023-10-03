dcac57fdb7de027a165ebe3f33522b9319c1eb9d
Bruno Martucci <brunomartucci77@gmail.com>
2023-10-03 12:41:56 -0300

    Segundo commit = Melhorei o codigo utilizando, construtores, this, sbrecarga, encapsulamento e metodo get e set

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,12 +1,3 @@
-/*
- * Programa aonde entro com dados do produtos como nome, valor e quantidade.
- * Consigo incluir e remover a quantidade do produto e quando realizo algum tipo
- * de alteraçao na quantidade o programa me retorna a quantidade e o valor atualizado.
- */
-
-
-
-
 package application;
 
 import java.util.Locale;
@@ -22,14 +13,18 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product product = new Product();
+		
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		double price = sc.nextDouble();
+		Product product = new Product(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
